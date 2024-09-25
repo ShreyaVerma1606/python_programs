5abdd0479d82dd929a7c43e2c3261bb3b8145927
ShreyaVerma1606 <vermashreya1606@gmail.com>
2024-09-25 12:01:11 -0700

    Update python.py

--- python.py
+++ python.py
@@ -33,17 +33,6 @@ print(f"My name is {n} and email is {e} {s}")
 # by=int(input("born year:"))
 # cy=int(input("current year"))
 # print("your age is:",cy-by,"years")
-# #while loop
-# create function to print the name
-def printName():
-    print("My name is shreya")
-# call function to print name
-printName()
-# create func.to concatenate fname and lname from
-fname= input("enter your first name:")
-lname= input("enter your last name:")
-def printFullName(firstname,lastname):
-    print(firstname+" "+lastname)
-printFullName(fname,lname)
+
 
     
