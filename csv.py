2cfd16203bc332363fbb07421c6b02376c3ac896
ShreyaVerma1606 <vermashreya1606@gmail.com>
2024-10-05 09:56:44 -0700

    Add files via upload

---
+++ csv.py
@@ -0,0 +1,7 @@
+#create read write delete csv, excel, json, text
+#create csv 
+mycsv = open("myfile.csv","w")
+mycsv.write("Shreya,Vishu, Apu, Anshu")
+
+myexcel = open("myexcel.xlsx","w")
+myexcel.write("Dolly, Nupur, Neha, Ishu")
